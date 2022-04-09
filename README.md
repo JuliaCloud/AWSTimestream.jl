ac425b156eb2dec45f28c9b6a144bbe0b984d011
klangner <klangner@gmail.com>
2022-04-09 13:26:45 +0200

    Firat working version. Query supported.  @JuliaRegistrator register

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Timestream
+# AWS Timestream client
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://klangner.github.io/Timestream.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://klangner.github.io/Timestream.jl/dev)
 [![Build Status](https://github.com/klangner/Timestream.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/klangner/Timestream.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/klangner/Timestream.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/klangner/Timestream.jl)
+
+`Timestream` is an early-stage open-source project. It means that API can change at any time. If you think that this library can help you, then let me know. We can discuss future direction and try to stabilize the API.
+
+Currently only query is supported
--- src/Timestream.jl
+++ src/Timestream.jl
@@ -1,5 +1,48 @@
 module Timestream
 
-# Write your package code here.
+using AWS: @service, JSONService
+using AWS.UUIDs
+
+@service Timestream_Query
+
+export init_query_client, query
+
+"""
+    init_query_client()
+This function will query for correct endpoint and then initialize client.
+Please remember that this client has limited TTL. Usually 24h.
+So if the query command fail, try to aquire new client.
+"""
+function init_query_client()
+    ep_info = Timestream_Query.describe_endpoints()
+    endpoints = first(ep_info)[2]
+    a = endpoints[1]["Address"]
+    uri = a[1:findfirst('.', a)] * "timestream"
+    JSONService(
+        "timestream", uri, "2018-11-01", "1.0", "Timestream_20181101"
+    )
+end
+
+
+"""
+    query(client, q)
+Execute given query on Timestream and return all result records
+This function will read all pages with data before returning the result.
+
+# Arguments
+- `client`: client created with `init_query_client` function
+- `q`: Query string which specifies what should be returned (SQL command)
+"""
+function query(client, q)
+    client_token = string(uuid4())
+    result = client("Query", Dict{String,Any}("QueryString" => q, "ClientToken" => client_token))
+    rows = result["Rows"]
+    while haskey(result, "NextToken")
+        result = client("Query", Dict{String,Any}("QueryString" => q, "ClientToken" => client_token, "NextToken" => result["NextToken"]))
+        rows = [rows; result["Rows"]]
+    end
+
+    rows
+end
 
 end
