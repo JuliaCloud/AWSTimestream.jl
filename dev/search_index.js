ab1a369dc2b849bb342b8346d0cf97e7cfe62c04
Documenter.jl <documenter@juliadocs.github.io>
2022-04-09 10:13:16 +0000

    build based on ff005ea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Timestream","category":"page"},{"location":"#Timestream","page":"Home","title":"Timestream","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Timestream.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Timestream]","category":"page"}]
+}
